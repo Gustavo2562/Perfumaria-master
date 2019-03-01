03e3d7f26478f98336b4c0306882dc8a481f9d8b
Gabrielti2018 <gabriel_ferreira.99@hotmail.com>
2019-03-01 11:23:03 -0300

    novo site

--- masculino.php
+++ masculino.php
@@ -117,11 +117,10 @@
         
         <nav aria-label="Page navigation example">
   <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Voltar</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Avançar</a></li>
+      <li class="page-item"><a class="page-link" href="mas1.php">Voltar</a></li>
+    <li class="page-item"><a class="page-link" href="mas1.php">1</a></li>
+    <li class="page-item"><a class="page-link" href="mas2.php">2</a></li>
+    <li class="page-item"><a class="page-link" href="mas2.php">Avançar</a></li>
   </ul>
 </nav>
     </body>
